107bade8c7b76747f89a291a48c74296feda0d26
flipolasagna <111539493+flipolasagna@users.noreply.github.com>
2022-10-14 14:34:34 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
 #define bot1 ((PD4>>4)& 0X01)
-#define bot1 ((PD4>>5)& 0X01)
-#define bot1 ((PD4>>6)& 0X01)
-#define bot1 ((PD4>>7)& 0X01)
+#define bot2 ((PD5>>5)& 0X01)
+#define bot3 ((PD6>>6)& 0X01)
+#define bot4 ((PD7>>7)& 0X01)
 
 #define prender(PORT, PIN) (PORT |= 1 << PIN)
 #define apagar(PORT, PIN) (PORT &= 1 << PIN)
 
+
+
+int main()
+ 
 DDRD &= ~(1 << PD4);
 DDRD &= ~(1 << PD5);
 DDRD &= ~(1 << PD6);
 DDRD &= ~(1 << PD7);
-
 
 DDRD |= (1 << PD2);
 DDRD |= (1 << PD3);
@@ -22,6 +25,6 @@ DDRB |= (1 << PB2);
 DDRB |= (1 << PB3);
 DDRB |= (1 << PB4);
 
-int main()
-  
-while (1) 
+while(1)
+{
+
